8219b1e73809057b224341d7326a17735690be8c
MccGrey <chuxzzie01@yahoo.com>
2022-10-23 08:20:20 +0100

    add navbar responsive design

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,18 +1,43 @@
+import { useRef } from "react";
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
 import "./navbar.css";
+import { FaUserAlt } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
 
 export default function Navbar() {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive-nav");
+  };
+
   return (
     <div className="nav-container">
       <nav className="container nav">
         <Link to="/" className="logo">
           LOGO
         </Link>
-        <ul>
+
+        <ul ref={navRef}>
           <CustomLink to="/">Home</CustomLink>
           <CustomLink to="/alcoholicDrinks">Alcoholic Drinks</CustomLink>
           <CustomLink to="/nonAlcoholicDrinks">Non-Alcoholic Drinks</CustomLink>
+
+          <button className="nav-btn close-nav" onClick={showNavbar}>
+            <AiOutlineClose />
+          </button>
         </ul>
+
+        <div className="nav-icons">
+          <FaUserAlt />
+          <FaShoppingCart />
+        </div>
+
+        <button className="nav-btn open-nav" onClick={showNavbar}>
+          <GiHamburgerMenu />
+        </button>
       </nav>
     </div>
   );
