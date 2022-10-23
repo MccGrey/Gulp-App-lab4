c196ba3464501b70e46527a793ad5a21490175fc
MccGrey <chuxzzie01@yahoo.com>
2022-10-23 14:18:58 +0100

    add search bar

---
+++ src/components/searchbar/Search.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./search.css";
+import { RiSearch2Fill } from "react-icons/ri";
+
+const Search = () => {
+  return (
+    <div className="search-container">
+      <select>
+        <option value="grape">Graope fruit</option>
+        <option value="grape">Graope fruit</option>
+        <option selected value="grape">
+          Graope fruit
+        </option>
+        <option value="grape">Graope fruit</option>
+      </select>
+
+      <input type="search" className="search-box" />
+      <button className="search-icon">
+        {" "}
+        <RiSearch2Fill />
+      </button>
+    </div>
+  );
+};
+
+export default Search;
