d82bdd5d1e3225c7d02013cf80bbe0b198c63214
MccGrey <chuxzzie01@yahoo.com>
2022-10-25 11:28:01 +0100

    testimonial swiper

--- src/components/testimonials/Testimonials.js
+++ src/components/testimonials/Testimonials.js
@@ -5,62 +5,71 @@ import TEST2 from "../../assets/gulp pt wom 1.png";
 import TEST3 from "../../assets/Gulp Ptrait one 1.png";
 import { RiDoubleQuotesL } from "react-icons/ri";
 import { IoMdStar } from "react-icons/io";
-import { IoMdStarOutline } from "react-icons/io";
-import { IoMdStarHalf } from "react-icons/io";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper";
 
 const Testimonials = () => {
+  const data = [
+    {
+      id: 1,
+      avatr: TEST3,
+      pname: "Angela Koya",
+      review: "Gulp always leaves me in awe with their swift delivery service",
+    },
+
+    {
+      id: 2,
+      avatr: TEST1,
+      pname: "Remy Johnny",
+      review: "I love that I can get varieties of drink everytime",
+    },
+
+    {
+      id: 1,
+      avatr: TEST2,
+      pname: "Sandra Kent",
+      review: "I cant count how many of my friends I showed this site to.",
+    },
+  ];
   return (
     <section className="testimonials">
       <div className="testimonial-heading">
         <h3>What our clients are saying</h3>
         <div className="bar"></div>
       </div>
-      <div className="testimonials-container">
-        <article className="testimonial-card">
-          <RiDoubleQuotesL className="icon-quote" />
-          <h3>
-            Gulp always leaves me in awe with their swift delivery service
-          </h3>
-          <img src={TEST3} alt="testimonial1" />
-          <h5>Angela Koya</h5>
-          <div className="client-review">
-            <IoMdStar />
-            <IoMdStar />
-            <IoMdStar />
-            <IoMdStar />
-            <IoMdStar />
-          </div>
-        </article>
-
-        <article className="testimonial-card">
-          <RiDoubleQuotesL className="icon-quote" />
-          <h3>I love that I can get varieties of drink everytime</h3>
-          <img src={TEST1} alt="testimonial2" />
-          <h5>Remy Johnny</h5>
-          <div className="client-review">
-            <IoMdStar />
-            <IoMdStar />
-            <IoMdStar />
-            <IoMdStar />
-            <IoMdStarHalf />
-          </div>
-        </article>
-
-        <article className="testimonial-card">
-          <RiDoubleQuotesL className="icon-quote" />
-          <h3>I cant count how many of my friends I showed this site to.</h3>
-          <img src={TEST2} alt="testimonial3" />
-          <h5>Sandra Kent</h5>
-          <div className="client-review">
-            <IoMdStar />
-            <IoMdStar />
-            <IoMdStar />
-
-            <IoMdStarHalf />
-            <IoMdStarOutline />
-          </div>
-        </article>
-      </div>
+      <Swiper
+        className="testimonials-container"
+        pagination={{
+          type: "progressbar",
+        }}
+        slidesPerView={1}
+        spaceBetween={40}
+        modules={[Pagination]}
+      >
+        {data.map(({ avatr, pname, review }, index) => {
+          return (
+            <SwiperSlide key={index} className="testimonial-card">
+              <RiDoubleQuotesL className="icon-quote" />
+              <h3>{review}</h3>
+              <img src={avatr} alt="testimonial1" />
+              <h5>{pname}</h5>
+              <div className="client-review">
+                <IoMdStar />
+                <IoMdStar />
+                <IoMdStar />
+                <IoMdStar />
+                <IoMdStar />
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </section>
   );
 };
