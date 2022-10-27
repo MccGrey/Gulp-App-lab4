d0e17e8a82d5c103e3578bb52ddd4159834fb16d
MccGrey <chuxzzie01@yahoo.com>
2022-10-27 07:00:15 +0100

    add signup content and styling

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,13 +1,21 @@
 import React from "react";
+import Footer from "../../components/footer/Footer";
+import "./cart.css";
 
 const Cart = () => {
   return (
-    <div>
-      <br /> <br />
-      <br />
-      <br />
-      <br /> Cart
-    </div>
+    <>
+      <section className="cart-section">
+        <div className="cart-container">
+          <div className="cart-bar"></div>
+          <div className="navigator">
+            <h3 className="cart-head">Cart</h3>
+            <h3 className="checkout-head">Checkout</h3>
+          </div>
+        </div>
+        <Footer />
+      </section>
+    </>
   );
 };
 
