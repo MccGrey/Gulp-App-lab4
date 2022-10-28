54a1d915953caac30cbf2297fe01799fbbc00284
MccGrey <chuxzzie01@yahoo.com>
2022-10-28 11:48:21 +0100

    product detail - details styling

--- src/App.js
+++ src/App.js
@@ -11,6 +11,13 @@ import Cart from "./components/cart/Cart";
 import Signup from "./components/signup/Signup";
 import Checkout from "./components/checkout/Chekout";
 import Details from "./components/details/Details";
+import Detail2 from "./components/detail2/Detail2";
+import Detail3 from "./components/detail3/Detail3";
+import Detail4 from "./components/detail4/Detail4";
+import Detail5 from "./components/detail5/Detail5";
+import Detail6 from "./components/detail6/Detail6";
+import Detail7 from "./components/detail7/Detail7";
+import Detail8 from "./components/detail8/Detail8";
 
 const App = () => {
   return (
@@ -24,6 +31,13 @@ const App = () => {
           <Route path="/nonAlcoholicDrinks" element={<NonAlcoholicDrinks />} />
           <Route path="/shop" element={<Shop />} />
           <Route path="/product" element={<Details />} />
+          <Route path="/product2" element={<Detail2 />} />
+          <Route path="/product3" element={<Detail3 />} />
+          <Route path="/product4" element={<Detail4 />} />
+          <Route path="/product5" element={<Detail5 />} />
+          <Route path="/product6" element={<Detail6 />} />
+          <Route path="/product7" element={<Detail7 />} />
+          <Route path="/product8" element={<Detail8 />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/cart" element={<Cart />} />
---
+++ src/components/detail5/Detail5.js
@@ -0,0 +1,57 @@
+import React from "react";
+import DET5 from "../../assets/CHIVITA APPLLE JUICE 100CL.png";
+
+const detail5 = () => {
+  return (
+    <>
+      <section className="product-detail-section">
+        <div className="cart-container detail-main-conatiner">
+          <div className="cart-bar detail-line"></div>
+          <h3 className="main-heading">Product Details</h3>
+          <div className="details-container">
+            <div className="detail-img">
+              <img src={DET5} alt="CHIVITA APPLLE JUICE 100CL" />
+            </div>
+            <div className="detail-main-main">
+              <div className="details-heading">
+                <h3 className="title">CHIVITA APPLLE JUICE 100CL</h3>
+                <h3 className="quantity">Quantity</h3>
+                <h3 className="amount">Amount</h3>
+              </div>
+              <div className="details-content">
+                <div className="detail-package">
+                  <small>single</small>
+                  <h3>x12</h3>
+                </div>
+                <div className="detail-item-quantity">
+                  <button>-</button>
+                  <div className="count">5</div>
+                  <button>+</button>
+                </div>
+                <div className="deatil-amount">N50000</div>
+              </div>
+              <div className="detailed-description">
+                <h4>Description</h4>
+                <p>
+                  Hennessy V.S.O.P. is medium amber with fine aromas of sweet
+                  fruit, honey, caramel, cloves, nuts and oaky notes. The palate
+                  is round with smooth fruity flavours of orange peel, prune and
+                  vanilla on a long lingering finish. Hennessy V.S.O.P. is a 4
+                  year old cognac, aged in Limousin oak barrels. A world famous
+                  cognac producer, Hennessy has a history dating back to 1765
+                  when the company was established by Irishman Richard Hennessy.
+                  In the beginning, it was an eaux-de-vie trading business, but
+                  Hennessy was going to become the most successful cognac
+                  exporter in the world.
+                </p>
+              </div>
+            </div>
+          </div>
+          <button className="detail-addtocart">Add to cart</button>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default detail5;
